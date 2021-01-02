c174312a545f702d4e98bd3fa9d3c358b01db266
AccessDeniedx <13848638462@163.com>
2021-01-02 20:36:54 +0800

    :high_heel: 1.2 commit :sparkles:

--- app/src/main/java/com/example/androiddevelopmentfinal/Person/PersonFragment.java
+++ app/src/main/java/com/example/androiddevelopmentfinal/Person/PersonFragment.java
@@ -1,42 +1,65 @@
 package com.example.androiddevelopmentfinal.Person;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.ContentUris;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.ContactsContract;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.widget.ActionBarContainer;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.androiddevelopmentfinal.R;
 import com.leon.lib.settingview.LSettingItem;
 
 import com.leon.lib.settingview.LSettingItem;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class PersonFragment extends Fragment {
 
     View view;
+    public static final int TAKE_PHOTO = 1;
+    public static final int CHOOSE_PHOTO = 2;
+    private Uri imageUri;
+    CircleImageView user_image;
+    LSettingItem force_Offline;
+    LSettingItem read_contact;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        this.view = inflater.inflate(R.layout.person_fragment, container, false);
+        findViews();
 
-        view = inflater.inflate(R.layout.person_fragment, container, false);
-        Log.d("FirstActivity", "Task id is " + getActivity().getTaskId());//singleinstance打印当前返回栈的id
-
-        return view;
+        return this.view;
     }
 
     @Override
@@ -53,11 +76,9 @@ public class PersonFragment extends Fragment {
             }
         });*/
         /*强制下线*/
-        LSettingItem force_Offline = (LSettingItem) view.findViewById(R.id.force_Offline);
         force_Offline.setmOnLSettingItemClick(new LSettingItem.OnLSettingItemClick() {
             @Override
             public void click() {
-                /*Toast.makeText(getActivity(),"You clicked Add",Toast.LENGTH_SHORT).show();*/
                 Intent intent = new Intent("com.example.androiddevelopmentfinal." +
                         "FORCE_OFFLINE");
                 getActivity().sendBroadcast(intent);
@@ -66,11 +87,9 @@ public class PersonFragment extends Fragment {
         });
 
         /*读取联系人*/
-        LSettingItem read_contact = (LSettingItem) view.findViewById(R.id.read_contacts);
         read_contact.setmOnLSettingItemClick(new LSettingItem.OnLSettingItemClick() {
             @Override
             public void click() {
-
                 Intent intent = new Intent(getActivity(), Contacts_Reader.class);
                 startActivity(intent);
             }
@@ -78,7 +97,6 @@ public class PersonFragment extends Fragment {
         });
 
         /*更换头像*/
-        CircleImageView user_image = (CircleImageView) view.findViewById(R.id.user_image);
         user_image.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -87,6 +105,12 @@ public class PersonFragment extends Fragment {
         });
 
 
+    }
+
+    private void findViews(){
+        user_image = (CircleImageView) view.findViewById(R.id.user_image);
+        force_Offline = (LSettingItem) view.findViewById(R.id.force_Offline);
+        read_contact = (LSettingItem) view.findViewById(R.id.read_contacts);
     }
 
     private void showTypeDialog() {
@@ -99,18 +123,144 @@ public class PersonFragment extends Fragment {
         tv_select_gallery.setOnClickListener(new View.OnClickListener() {// 在相册中选取
             @Override
             public void onClick(View v) {
-
+                choose_photo_to_image();
                 dialog.dismiss();
             }
         });
         tv_select_camera.setOnClickListener(new View.OnClickListener() {// 调用照相机
             @Override
             public void onClick(View v) {
-
+                take_photo_to_image();
                 dialog.dismiss();
             }
         });
+
         dialog.setView(view);
         dialog.show();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode,int resultCode, Intent data){
+        switch (requestCode){
+            case TAKE_PHOTO:
+                if(resultCode == getActivity().RESULT_OK){
+                    try {
+                        Bitmap bitmap = BitmapFactory.decodeStream(getActivity().
+                                getContentResolver().openInputStream(imageUri));
+                        user_image.setImageBitmap(bitmap);
+                    }catch (FileNotFoundException e){
+                        e.printStackTrace();
+                    }
+                }
+                break;
+            case CHOOSE_PHOTO:
+                if(resultCode == getActivity().RESULT_OK){
+                    if(Build.VERSION.SDK_INT >=19 ){
+                        handleImageOnKitKat(data);
+                    }else {
+                        handleImageBeforeKitKat(data);
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
+    private void take_photo_to_image(){
+        File outputImage = new File(getActivity().getExternalCacheDir(),"output_image.jpg");
+        try {
+            if(outputImage.exists()){
+                outputImage.delete();
+            }
+            outputImage.createNewFile();
+        }catch (IOException e){e.printStackTrace();}
+        if(Build.VERSION.SDK_INT >= 24){
+            imageUri = FileProvider.getUriForFile(getActivity(),
+                    "com.example.androiddevelopmentfinal.fileprovider",outputImage);
+        }else {
+            imageUri = Uri.fromFile(outputImage);
+        }
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+        startActivityForResult(intent,TAKE_PHOTO);
+    }
+    private void choose_photo_to_image(){
+        if(ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.
+                WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(getActivity(),new String[]{Manifest.
+                    permission.WRITE_EXTERNAL_STORAGE}, 1);
+        }else {
+            openAlbum();
+        }
+    }
+
+    private void openAlbum(){
+        Intent intent = new Intent("android.intent.action.GET_CONTENT");
+        intent.setType("image/*");
+        startActivityForResult(intent,CHOOSE_PHOTO);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String[] peimissions,int[] grantResults){
+        switch (requestCode){
+            case 1:
+                if(grantResults.length > 0 && grantResults[0] ==
+                        PackageManager.PERMISSION_GRANTED){
+                    openAlbum();
+                }else {
+                    Toast.makeText(getActivity(),"You denied the permission",
+                            Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+        }
+    }
+
+    @TargetApi(19)
+    private void handleImageOnKitKat(Intent data){
+        String imagePath = null;
+        Uri uri = data.getData();
+        if(DocumentsContract.isDocumentUri(getActivity(),uri)) {
+            String docId = DocumentsContract.getDocumentId(uri);
+            if("com.android.providers.media.documents".equals(uri.getAuthority())){
+                String id = docId.split(":")[1];
+                String selection = MediaStore.Images.Media._ID+"="+id;
+                imagePath = getImagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,selection);
+            }else if("com.android.providers.downloads.documents".equals(uri.getAuthority())){
+                Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"),Long.valueOf(docId));
+                imagePath = getImagePath(contentUri,null);
+            }
+        }else if ("content".equalsIgnoreCase(uri.getScheme())){
+            imagePath = getImagePath(uri,null);
+        }else if("file".equalsIgnoreCase(uri.getScheme())){
+            imagePath = uri.getPath();
+        }
+        displayImage(imagePath);
+    }
+
+    private void handleImageBeforeKitKat(Intent data){
+        Uri uri = data.getData();
+        String imagePath = getImagePath(uri,null);
+        displayImage(imagePath);
+    }
+
+    private String getImagePath(Uri uri,String selection){
+        String path = null;
+        Cursor cursor = getActivity().getContentResolver().query(uri,null,selection,null,null);
+        if(cursor != null){
+            if(cursor.moveToFirst()){
+                path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            }
+        }
+        return path;
+    }
+
+    private void displayImage(String imagePath){
+        if(imagePath != null){
+            Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
+            user_image.setImageBitmap(bitmap);
+        }else {
+            Toast.makeText(getActivity(),"failed to get image",Toast.LENGTH_SHORT).show();
+        }
     }
 }
