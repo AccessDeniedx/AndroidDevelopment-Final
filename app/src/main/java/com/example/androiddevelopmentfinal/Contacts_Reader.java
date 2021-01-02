68b0d1d30c07c8baa29050ac75386b843d793d8c
AccessDeniedx <13848638462@163.com>
2021-01-02 15:48:58 +0800

    :high_heel: 1.2 commit :sparkles:

---
+++ app/src/main/java/com/example/androiddevelopmentfinal/Contacts_Reader.java
@@ -0,0 +1,14 @@
+package com.example.androiddevelopmentfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Contacts_Reader extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts_reader);
+    }
+}
